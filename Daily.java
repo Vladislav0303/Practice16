fd55514a01b63a4616cc6a9017cffb850a638797
Vladislav0303 <vladshulyak003@gmail.com>
2025-05-19 15:57:44 +0300

    Adding Practice work

---
+++ Daily.java
@@ -0,0 +1,25 @@
+package some.packing.bibika.bika;
+import some.packing.bibika.bika.utils.FileUtils;
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+public class Daily {
+        public static void main(String[] args) {
+            menu();
+        }
+        public static void menu() {
+            LocalDateTime[] massdat = new LocalDateTime[5];
+            String[] str1 = new String[5];
+            Scanner sc = new Scanner(System.in);
+            System.out.println("~Мій щоденник~");
+            System.out.println("Введіть куди записувати: ");
+            String name = sc.nextLine();
+            System.out.println("Виберіть пункт: ");
+            int c = sc.nextInt();
+            if(c == 1) {
+                FileUtils.mynote(massdat, str1, name);
+            } else if(c == 2) {
+                FileUtils.diary(massdat,str1,name);
+            }
+        }
+}
